073dd73c5c7ed8a0fda7f73641fbc3879a30c38e
FIVE <cahhong@artfx.fr>
2024-05-31 02:59:12 +0200

    Added more power to clear.py

--- clear.py
+++ clear.py
@@ -12,3 +12,5 @@ for folder_path in folders:
         file_path = os.path.join(folder_path, filename)
         if os.path.isfile(file_path):
             os.remove(file_path)
+        elif os.path.isdir(file_path):
+            os.rmdir(file_path)
